eb08b954d50d0dfb56d5bc4a9b3244b1d76dcd25
Roan Ratering <roan.ratering@bsi-software.com>
2024-07-04 09:58:00 +0200

    .

--- gitgpt.sh
+++ gitgpt.sh
@@ -7,15 +7,6 @@ if [ ! -d .git ]; then
   exit 1
 fi
 
-<<<<<<< HEAD
-=======
-# Create .gitignore if it does not exist
-if [[ ! -f "$GITIGNORE" ]]; then
-  touch "$GITIGNORE"
-  echo "# GitIgnore File" >> "$GITIGNORE"
-fi
-
->>>>>>> 384c4be62c6c3bc1174863b8c416f6f689b00dde
 # Check if .env exists and contains API_KEY
 if [[ ! -f "$ENV_FILE" ]]; then
   echo "Please enter your OpenAI API key:"
@@ -28,7 +19,6 @@ else
   if [[ -z "${OPENAI_API_KEY}" ]]; then
     echo "Please enter your OpenAI API key:"
     read -r API_KEY
-<<<<<<< HEAD
     echo "OPENAI_API_KEY=$API_KEY" >"$ENV_FILE"
 else
     source "$ENV_FILE"
@@ -37,10 +27,6 @@ else
         read -r API_KEY
         echo "OPENAI_API_KEY=$API_KEY" >>"$ENV_FILE"
     fi
-=======
-    echo "OPENAI_API_KEY=$API_KEY" >> "$ENV_FILE"
-  fi
->>>>>>> 384c4be62c6c3bc1174863b8c416f6f689b00dde
 fi
 
 # Load the API key
@@ -48,7 +34,6 @@ source "$ENV_FILE"
 
 # Function to check if the API key is valid
 function check_api_key() {
-<<<<<<< HEAD
     RESPONSE=$(curl -s -o /dev/null -I -w "%{http_code}" -H "Authorization: Bearer $OPENAI_API_KEY" "$API_URL")
     while [[ "$RESPONSE" != "200" ]]; do
         echo "API key is not valid. Please enter a valid OpenAI API key:"
@@ -57,15 +42,6 @@ function check_api_key() {
         RESPONSE=$(curl -s -o /dev/null -I -w "%{http_code}" -H "Authorization: Bearer $OPENAI_API_KEY" "$API_URL")
     done
     echo "API key is valid."
-=======
-  RESPONSE=$(curl -s -o /dev/null -I -w "%{http_code}" -H "Authorization: Bearer $OPENAI_API_KEY" "https://api.openai.com/v1/engines")
-  while [[ "$RESPONSE" != "200" ]]; do
-    echo "API key is not valid. Please enter a valid OpenAI API key:"
-    read -r OPENAI_API_KEY
-    RESPONSE=$(curl -s -o /dev/null -I -w "%{http_code}" -H "Authorization: Bearer $OPENAI_API_KEY" "https://api.openai.com/v1/engines")
-  done
-  echo "API key is valid."
->>>>>>> 384c4be62c6c3bc1174863b8c416f6f689b00dde
 }
 
 check_api_key
